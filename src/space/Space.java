2de5db6ce8233b9d14fa03825fcd0cf8f5024cb2
vinicius <viniciuscrato123@gmail.com>
2015-10-30 01:48:53 -0200

    Subindo telas

--- src/space/Space.java
+++ src/space/Space.java
@@ -5,12 +5,10 @@
  */
 package space;
 
-import java.awt.Color;
-import javax.swing.JOptionPane;
-import jplay.GameImage;
-import jplay.Keyboard;
-import jplay.URL;
-import jplay.Window;
+import java.awt.event.KeyEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jplay.*;
 
 /**
  *
@@ -24,36 +22,65 @@ public class Space {
     public static void main(String[] args) {
         // TODO code application logic here
         Window janela = new Window(1280, 720);
-        GameImage imgMenu = new GameImage(URL.sprite("TelaInicial.png"));
+        GameImage imgMenu;
         Keyboard teclado = janela.getKeyboard();
-        Cenario cenario = new Cenario(janela);
-        int newGame;
-        boolean primeiraVez = true;
+        Cenario cenario;
+//        boolean menuPlayer = true, menuMulti, menuSair, parada1, parada2, parada3;
+        boolean loop = true;
+        int menu = 1;
+        imgMenu = new GameImage(URL.sprite("menuPlayer.png"));
 
-        while (true) {
+        while (loop) {
             imgMenu.draw();
             janela.update();
-            if (teclado.keyDown(Keyboard.ENTER_KEY)) {
 
-                if (primeiraVez) {
+            if (teclado.keyDown(KeyEvent.VK_DOWN)) {
+                switch (menu) {
+                    case 1:
+                        imgMenu = new GameImage(URL.sprite("menuMulti.png"));
+                        menu = 2;
+                        break;
 
-                    cenario.run();
-                    primeiraVez = false;
-                    
-                }else{
-                    newGame = JOptionPane.showConfirmDialog(null, "Deseja Continuar:", "Jogo", 0);
-                    if (newGame == 1) {
-                        cenario = new Cenario(janela);
-                        cenario.run();
-                    } else {
-                        cenario.run();
-                    }
+                    case 2:
+                        imgMenu = new GameImage(URL.sprite("menuSair.png"));
+                        menu = 0;
+                        break;
                 }
+            }
 
+            if (teclado.keyDown(KeyEvent.VK_UP)) {
+                switch (menu) {
+                    case 2:
+                        imgMenu = new GameImage(URL.sprite("menuPlayer.png"));
+                        menu = 1;
+                        break;
+
+                    case 0:
+                        imgMenu = new GameImage(URL.sprite("menuMulti.png"));
+                        menu = 2;
+                        break;
+                }
             }
-            if (teclado.keyDown(Keyboard.ESCAPE_KEY)) {
-                janela.exit();
+
+            if (teclado.keyDown(KeyEvent.VK_ENTER)) {
+                switch (menu) {
+                    case 1:
+                        cenario = new Cenario(janela);
+                        break;
+
+                    case 2:
+                        //não faz nada
+                        break;
+
+                    case 0:
+                        loop = false;
+                        janela.exit();
+                        break;
+                }
             }
+
         }
+
     }
+
 }
