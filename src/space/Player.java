19846427941d10b1e67188337fa99204ecf42121
vinicius <viniciuscrato123@gmail.com>
2015-11-12 18:54:08 -0200

    Atualizando a classe conexão

---
+++ src/space/Player.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package space;
+
+/**
+ *
+ * @author batista
+ */
+public class Player {
+    private String nome;
+    private int points;
+
+    public Player() {
+    
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+    
+    
+    
+}
