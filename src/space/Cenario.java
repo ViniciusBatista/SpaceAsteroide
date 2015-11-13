dc76831c77e704a40e3066d1c686f6d888d6e09f
vinicius <viniciuscrato123@gmail.com>
2015-11-13 01:29:55 -0200

    Implementado o metodo para remover os asteroides depois depois que ele voltar para o menu principal

--- src/space/Cenario.java
+++ src/space/Cenario.java
@@ -105,6 +105,7 @@ public class Cenario {
                         break;
                     case 1:
                         sair = false;
+                        ConIni.deleteAsteroide();
                         break;
 
                 }
